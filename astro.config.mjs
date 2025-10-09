4cdaf0168697a42423a58887559e328052ddd2c5
Khambazarov <khambazarovrenat@gmail.com>
2025-10-09 21:06:50 +0200

    feat: configure vercel serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://fensterputzer.netlify.app", // <- anpassen, wichtig für Canonicals & Sitemap
+  site: "https://fensterputzer.netlify.app",
   integrations: [sitemap()],
+    adapter: vercel(),
   vite: {
     plugins: [tailwind()],
   },
